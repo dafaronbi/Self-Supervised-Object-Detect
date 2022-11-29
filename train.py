3f326a5d4f96649f215ac0bbc27a6a20213b75bf
dafaronbi <>
2022-11-28 21:29:41 -0500

    fix printing

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ writer = SummaryWriter()
 
 for epoch in range(epochs):
     running_loss = 0.0
-    for i, data in enumerate(training_loader, 0):
+    for ind, data in enumerate(training_loader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
 
@@ -83,15 +83,14 @@ for epoch in range(epochs):
         writer.add_scalar("Loss/label", label_loss, epoch)
         writer.add_scalar("Loss/bboxes", bbox_loss, epoch)
         writer.add_scalar("Loss/score", score_loss, epoch)      
-        writer.add_scalar("Loss/all", loss, epoch)
-        print(loss)       
+        writer.add_scalar("Loss/all", loss, epoch)    
         loss.backward()
         optimizer.step()
         
         # print statistics
         running_loss += loss.item()
-        if i % 100 == 99:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+        if ind % 100 == 99:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {ind + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
 
 writer.flush()
